d21ec6d227b1b5e0cc0dad22dd08b9b0ee81e735
Batuhan <kucukyildizbatuhan@gmail.com>
2023-08-21 22:22:14 +0300

    Initial Commit

---
+++ Medium-SwiftRealm/Medium_SwiftRealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  Medium_SwiftRealmApp.swift
+//  Medium-SwiftRealm
+//
+//  Created by Batuhan Küçükyıldız on 21.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Medium_SwiftRealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
